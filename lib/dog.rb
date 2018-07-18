d54a6e28da3091a06fb1eef3a1d90b012381010a
Kyle-Neale <kyle.a.neale@gmail.com>
2018-07-18 12:15:59 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+
+
+class Dog
+  # attr_accessor :color, :breed, :trait
+  #
+  # def initialize(color, breed, trait)
+  #   @color = color
+  #   @breed = breed
+  #   @trait = trait
+  # end
+
+end
+
+fido = Dog.new
+# ("White", "Italian Crossbreed", "Famous")
+
+snoopy = Dog.new
+# ("Black and White", "Beagle", "Lazy")
+
+lassie = Dog.new
+# ("Brownish-Gold, Black, and White", "Sable Collie", "Heroic")
